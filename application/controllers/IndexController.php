edeb73e4680bfc55465ec8585a6b132ec6f7ceb1
BinaryKitten <Kathryn@binarykitten.com>
2012-08-09 10:23:31 +0100

    base views, controllers & layout

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * IndexController.php
+ * 
+ * @author Kathryn Reeve <k.reeve@ctidigital.com>
+ */
+/**
+ * IndexController
+ *   - controller to handle registration of customers
+ *   
+ */
+class IndexController extends Zend_Controller_Action
+{
+    public function indexAction(){}
+}
