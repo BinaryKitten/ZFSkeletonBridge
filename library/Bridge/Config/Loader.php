9f0453443cfda0602ff33bb79cb7ae1176bd38d3
BinaryKitten <Kathryn@binarykitten.com>
2012-09-24 10:45:06 +0100

    Updated Config loader to allow pull from module location

--- library/Bridge/Config/Loader.php
+++ library/Bridge/Config/Loader.php
@@ -5,35 +5,48 @@
  *
  * @author Kathryn Reeve <kathryn@binarykitten.com>
  */
-class Bridge_Config_Loader {
-    public static function getConfig($key, $asClass=null)
+class Bridge_Config_Loader
+{
+
+    public static function getConfig($key, $module = null, $asClass=null)
     {
         $key        = strtolower($key);
-        $location   = APPLICATION_PATH. '/configs/'.$key;
+        if ($module !== null && $asClass == null && substr(strtolower($module),0,5)=='zend_') {
+            $asClass = $module;
+            $module = null;
+        }
+
+        $location   = APPLICATION_PATH;
+        if ($module !== null) {
+            $location = Zend_Controller_Front::getInstance()->getModuleDirectory($module);
+        }
+        $location .= DIRECTORY_SEPARATOR . 'configs' . DIRECTORY_SEPARATOR . $key;
+
 
         if (file_exists($location.'.config.php')) {
              $config = include($location .'.config.php');
-        } elseif (file_exists(APPLICATION_PATH. '/configs/'.$key.'/')) {
-            if (!file_exists($location .'/production.config.php')) {
+        } elseif (file_exists($location . DIRECTORY_SEPARATOR)) {
+            if (!file_exists($location . DIRECTORY_SEPARATOR .'production.config.php')) {
                 throw new Zend_Config_Exception('Cannot locate config file for '.$key);
             }
-            $config = include($location .'/production.config.php');
+            $config = include($location . DIRECTORY_SEPARATOR . 'production.config.php');
 
             if (APPLICATION_ENV !== 'production') {
-                if (file_exists($location . '/'. APPLICATION_ENV . '.config.php')) {
+                if (file_exists($location . DIRECTORY_SEPARATOR . APPLICATION_ENV . '.config.php')) {
                     $config = Bridge_Stdlib_ArrayUtils::merge(
                         $config,
-                        include($location . '/'. APPLICATION_ENV . '.config.php')
+                        include($location . DIRECTORY_SEPARATOR . APPLICATION_ENV . '.config.php')
                     );
                 }
             }
-
         } else {
             throw new Zend_Config_Exception('Cannot locate config file for '.$key);
         }
-       
+
         if ($asClass !== null && class_exists($asClass)) {
-            $config = new $asClass($config);
+            if (class_exists($asClass)) {
+                $config = new $asClass($config);
+            }
         }
 
         return $config;
